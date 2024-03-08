ef30bd63f2565246c1ee03860cabcb25f8bbbeae
unknown <sohag.hossain.00013@gmail.com>
2024-03-08 12:34:56 +0600

    Chat with uploaded PDF

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,52 @@
 import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import google.generativeai as palm
+from langchain.embeddings import GooglePalmEmbeddings
+from langchain.llms import GooglePalm
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
 import os
 
-os.environ['GOOGLE_API_KEY'] =  'AIzaSyCpeOogkzyWebU0JxyrPu2mC4zK6UKb4j8'
+ 
 
 class chatbot_functionality:
-    pass
+    def __init__(self,api):
+        os.environ['GOOGLE_API_KEY'] = api
+    
+    def get_pdf_text(self,pdf_docs):
+        text=""
+        for pdf in pdf_docs:
+            pdf_reader= PdfReader(pdf)
+            for page in pdf_reader.pages:
+                text+= page.extract_text()
+        return  text
 
+    def get_text_chunks(self,text):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+        chunks = text_splitter.split_text(text)
+        return chunks
+
+    def get_vector_store(self,text_chunks):
+        embeddings = GooglePalmEmbeddings()
+        vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+        return vector_store
+
+    def get_conversational_chain(self,vector_store):
+        llm=GooglePalm()
+        memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
+        conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+        return conversation_chain
+
+    def user_input(self,user_question):
+        response = st.session_state.conversation({'question': user_question})
+        st.session_state.chatHistory = response['chat_history']
+        for i, message in enumerate(st.session_state.chatHistory):
+            if i%2 == 0:
+                st.write("Human:🚺 ", i,message.content)
+            else:
+                st.write("response: ✔️", i,message.content)
     
-    
-    
-    
+
     
--- run_chatbot.py
+++ run_chatbot.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import google.generativeai as palm
-
+from chatbot import chatbot_functionality
 
 st.set_page_config("ChatBot")
 st.header("Chat with Multiple uploaded PDF 💬")
@@ -9,7 +8,24 @@ if "conversation" not in st.session_state:
     st.session_state.conversation = None
 if "chatHistory" not in st.session_state:
     st.session_state.chatHistory = None
+    
+    
+api='AIzaSyCpeOogkzyWebU0JxyrPu2mC4zK6UKb4j8'
+chatbot_obj = chatbot_functionality(api)  # create class instance
 
+if user_question:
+    chatbot_obj.user_input(user_question)
+with st.sidebar:
+    st.title("Settings")
+    st.subheader("Upload your Documents")
+    pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
+    if st.button("Process"):
+        with st.spinner("Processing"):
+            raw_text     = chatbot_obj.get_pdf_text(pdf_docs)        # get text from pdf files
+            text_chunks  = chatbot_obj.get_text_chunks(raw_text)     # used chunk_size=1000, chunk_overlap=20
+            vector_store = chatbot_obj.get_vector_store(text_chunks) #
+            st.session_state.conversation = chatbot_obj.get_conversational_chain(vector_store)
+            st.success("Done")
     
     
     
